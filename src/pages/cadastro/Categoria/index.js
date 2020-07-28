a5f317481d0ffa16cd529d7584b724316a6f5411
Fúlvio <fulviocanducci@hotmail.com>
2020-07-28 14:47:49 -0300

    Slider Show

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -6,6 +6,14 @@ function CadastroCategoria() {
   return (
     <PageDefault>
       <h1>Cadastro de Categoria</h1>
+      <form>
+        <label>
+          Nome da Categoria:
+          <input type="text" />
+        </label>
+
+        <button>Cadastrar</button>
+      </form>
       <Link to="/">Home</Link>{' '}
     </PageDefault>
   );
