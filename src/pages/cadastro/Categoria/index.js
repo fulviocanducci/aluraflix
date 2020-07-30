8a0fdff65e3a67e1a283dfcd95ef80da0239d5d8
Fúlvio <fulviocanducci@hotmail.com>
2020-07-29 21:41:27 -0300

    FormField

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,19 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import FormField from '../../../components/FormField';
 import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
+  const initialValue = { nome: '', cor: '#000000', descricao: '' };
+  const [categoria, setCategoria] = useState(initialValue);
+  const [categorias, setCategorias] = useState([]);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCategoria({ ...categoria, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setCategorias([...categorias, { ...categoria }]);
+    setCategoria(initialValue);
+  };
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
-      <form>
-        <label>
-          Nome da Categoria:
-          <input type="text" />
-        </label>
+      <h1>Cadastro de Categoria{categoria && ': ' + categoria.nome}</h1>
+      <form onSubmit={handleSubmit}>
+        <FormField
+          name="nome"
+          label="Nome"
+          type="text"
+          value={categoria.nome}
+          onChange={handleChange}
+        />
+
+        <FormField
+          name="descricao"
+          label="Descrição"
+          type="textarea"
+          value={categoria.descricao}
+          onChange={handleChange}
+        />
+
+        <FormField
+          name="cor"
+          label="Cor"
+          type="color"
+          value={categoria.cor}
+          onChange={handleChange}
+        />
 
         <button>Cadastrar</button>
       </form>
+      <div>
+        <ul>
+          {categorias.map((c, i) => (
+            <li key={i}>
+              {c.nome} {c.descricao} {c.cor}
+            </li>
+          ))}
+        </ul>
+      </div>
       <Link to="/">Home</Link>{' '}
     </PageDefault>
   );
